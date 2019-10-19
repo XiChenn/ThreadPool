114f5eaeac2fbcd45e6d79524950421cb92dfd5b
Xi Chen <xichen.u@gmail.com>
2019-10-18 23:43:52 -0400

    add a diagram

---
+++ README.md
@@ -0,0 +1,2 @@
+Thread Pool:
+![alt text](https://raw.githubusercontent.com/XiChenn/Diagram/master/Thread%20Pool.png "Thread Pool Diagram")
--- src/main/java/com/example/threadpool/threadDemo/CallableDemo.java
+++ src/main/java/com/example/threadpool/threadDemo/CallableDemo.java
@@ -6,12 +6,32 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 public class CallableDemo {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Callable<String> callable = () -> String.format("The result is: %d", new Random().nextInt(200));
+    private static final class Task implements Callable<Integer> {
 
-        FutureTask<String> futureTask = new FutureTask<>(callable);
-        new Thread(futureTask).start();
-        String result = futureTask.get();
-        System.out.println(result);
+        @Override
+        public Integer call() throws Exception {
+            Thread.sleep(3000);
+            int sum = 0;
+            for (int i = 0; i < 100; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Task task2 = new Task();
+        FutureTask<Integer> futureTask2 = new FutureTask<>(task2);
+        new Thread(futureTask2).start();
+
+        Callable<String> task1 = () -> String.format("The result is: %d", new Random().nextInt(200));
+        FutureTask<String> futureTask1 = new FutureTask<>(task1);
+        new Thread(futureTask1).start();
+
+        String result1 = futureTask1.get();
+        System.out.println(result1);
+
+        int result2 = futureTask2.get();
+        System.out.println(result2);
     }
 }
